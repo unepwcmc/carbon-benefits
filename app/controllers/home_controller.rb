1fcd5c8e8e8789a51cb04ad201c7e1022a5e16e5
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-03-12 13:52:22 +0000

    copies the views from the original python application

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+  def data
+  end
+
+  def nojs
+  end
+
+  def ie6
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Carbontool::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'about' => 'home#about'
+  match 'data' => 'home#data'
+  match 'nojavascript' => 'home#nojs'
+  match 'ie6' => 'home#ie6'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -49,6 +53,7 @@ Carbontool::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
