5f9b4cf85a1675006c946b34add61717dc96d220
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-03-19 11:52:30 +0000

    passing layer ids between frontent and backend

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,18 +4,34 @@ class Work < ActiveRecord::Base
   has_many :layers
 
   def import_from_json(the_json)
-    parsed_layers = the_json
-    parsed_work = parsed_layers.shift
+    parsed_work = the_json.first
+    parsed_layers = the_json[1..the_json.length]
     #populate the work object
     self.summary = parsed_work['stats'].to_json
     #populate the layer objects
-    self.layers.build parsed_layers
+    build_layers = []
+    parsed_layers.each do |layer|
+      puts layer.inspect
+      if layer['id']
+        l = Layer.find(layer['id'])
+        l.update_attributes(layer)
+      else
+        build_layers<< layer
+      end
+    end
+    self.layers.build build_layers
+    self
   end
 
   def export_to_json
-    the_json = [{'polygons' => [], 'stats' => JSON.parse(summary)}]
-    layers.each{ |l| the_json << l.as_json }
-    the_json.as_json
+    work_json = {
+      :id => (id + BASE_ID).alphadecimal,
+      :polygons => [],
+      :stats => JSON.parse(summary),
+      :total => true
+    }.to_json
+    layers_json = layers.map(&:as_json).join(',')
+    '[' + work_json + ',' + layers_json + ']'
   end
 
   def to_json
