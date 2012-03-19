16c46122e9acc798cf7e1be6bd93e4bc6eacc68f
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-03-19 11:52:30 +0000

    passing layer ids between frontent and backend

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -3,10 +3,17 @@ class Layer < ActiveRecord::Base
 
   def as_json(options={})
     {
-      :id => id,
-      :polygons => polygons,
-      :stats => stats
-    }
+      'id' => id,
+      'polygons' => JSON.parse(polygons),
+      'stats' => JSON.parse(stats)
+    }.to_json
   end
 
+  def polygons=(polygons_ary)
+    write_attribute(:polygons, polygons_ary.to_json)
+  end
+
+  def stats=(stats_hash)
+    write_attribute(:stats, stats_hash.to_json)
+  end
 end
