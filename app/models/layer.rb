1b9d78770fe50609687ee7b9c18eabea494cc6bf
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-03-20 11:56:13 +0000

    unzipping and extracting meta data from shapefile

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -2,7 +2,12 @@ class Layer < ActiveRecord::Base
   belongs_to :work
   has_many :polygon_class_colours
   has_many :polygon_classes, :through => :polygon_class_colours
-  has_attached_file :user_layer_file
+  has_attached_file :user_layer_file,
+    :styles => {'meta-data' => []},
+    :processors => ['meta_data_extractor']
+  validates_attachment_content_type :user_layer_file,
+    :content_type => [ 'application/zip', 'application/json', 'text/csv', 'application/vnd.google-earth.kml+xml' ]
+  validates_attachment_size :user_layer_file, :less_than => 100.megabytes
 
   def user_layer_file_columns
     #need to parse columns from the uploaded file
