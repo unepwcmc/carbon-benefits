b647a98603c2e976df10c0b90a79247c30550c89
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-03-21 16:01:49 +0000

    Tests for Polygon::new

--- app/models/polygon.rb
+++ app/models/polygon.rb
@@ -1,10 +1,16 @@
 class Polygon
-  TABLENAME = :polygon
-  
-  #Model to access cartodb's polygons
-  attr_reader :cartodb_id, :name, :the_geom, :class_id, :layer_id
+  include ActiveRecord::AttributeAssignment
 
+  TABLENAME = :polygon
+
+  #Model to access cartodb's polygons
+  attr_accessor :cartodb_id, :name, :the_geom, :class_id, :layer_id
+
+  def initialize attributes = nil
+    assign_attributes(attributes, :without_protection => true) if attributes
+  end
+  
   def self.find cartodb_id
-    CartoDB::Connection.row TABLENAME, cartodb_id
+    new(CartoDB::Connection.row(TABLENAME, cartodb_id))
   end
 end
