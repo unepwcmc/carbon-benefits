9140644a466a33ca04afbfd85387980aaf087a98
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-03-21 16:01:49 +0000

    Tests for Polygon::new

--- test/models/polygon_spec.rb
+++ test/models/polygon_spec.rb
@@ -1,9 +1,21 @@
 require 'minitest_helper'
 
 describe Polygon do
+  describe "#new" do
+    it "should return a polygon object" do
+      @it = Polygon.new
+      assert_equal Polygon, @it.class
+    end
+
+    it "should return a polygon with a cartodb_id" do
+      @it = Polygon.new({:cartodb_id => 1234})
+      assert_equal 1234, @it.cartodb_id
+    end
+  end
+
   describe "#find" do
     before do
-      @cartodb_id = 1
+      @cartodb_id = 1234
       @cartodb_polygon = {:cartodb_id => @cartodb_id}
 
       @cartodb_connection = MiniTest::Mock.new
@@ -17,9 +29,9 @@ describe Polygon do
     after do
       @cartodb_connection.verify
     end
-    
+
     it "should return an object with the cartodb_id" do
-      assert_equal @cartodb_polygon, @it
+      assert_equal @cartodb_id, @it.cartodb_id
     end
   end
 end
