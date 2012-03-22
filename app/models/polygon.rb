3604dc2874cd51e944e6b24b740964f4a29b19a7
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-03-22 09:09:28 +0000

    initial implementation of polygon.save

--- app/models/polygon.rb
+++ app/models/polygon.rb
@@ -4,13 +4,21 @@ class Polygon
   TABLENAME = :polygon
 
   #Model to access cartodb's polygons
-  attr_accessor :cartodb_id, :name, :the_geom, :class_id, :layer_id
+  ATTRIBUTES = [ :cartodb_id, :name, :the_geom, :class_id, :layer_id ]
+  ATTRIBUTES.each do |attr| attr_accessor attr end
 
   def initialize attributes = nil
     assign_attributes(attributes, :without_protection => true) if attributes
   end
-  
+
   def self.find cartodb_id
     new(CartoDB::Connection.row(TABLENAME, cartodb_id))
+  end
+
+  def save
+    CartoDB::Connection.insert_row(TABLENAME, attributes)
+  end
+
+  def attributes
   end
 end
