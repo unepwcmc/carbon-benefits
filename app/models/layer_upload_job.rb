c312bd280d9c236bbc3a06e3f814e71aafc99b0f
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-03-22 12:36:30 +0000

    uploading the file into a new table in CartoDB

--- app/models/layer_upload_job.rb
+++ app/models/layer_upload_job.rb
@@ -2,15 +2,20 @@ class LayerUploadJob
   include Resque::Plugins::Status
 
   def perform
+    @layer = Layer.find(options['layer_id'])
+    @layer_file = @layer.user_layer_file
+
     #first upload raw file to cartodb
-    #TODO
-    puts 'upoloading...'
+    puts "uploading to new table"
+    res = CartoDB::Connection.create_table '', @layer_file.to_file
+    puts res.inspect
+    @table_name = res[:name]
     #now do the copying and stuff into polygons table
-    puts 'copying'
+    puts 'copying to polygons'
     #TODO
     #now delete the raw table
     #TODO
-    puts 'deleting'
+    puts 'deleting the new table'
   end
 
 end
