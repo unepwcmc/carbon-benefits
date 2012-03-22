f55d2f641ea345fe9579a8ab66e2f118616c3881
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-03-22 11:17:28 +0000

    added resque to move cartodb manipulation thing to offline

--- app/controllers/layers_controller.rb
+++ app/controllers/layers_controller.rb
@@ -2,5 +2,12 @@ class LayersController < ApplicationController
 
   def select_fields
     @layer = Layer.find(params[:id])
+    job_params = {
+      :name_field => params[:name],
+      :class_field => params[:class],
+      :layer => @layer
+    }
+    job_id = LayerUploadJob.create(job_params)
+    #return this id to the front end and tie it to the layer tab somehow to show progress
   end
 end
