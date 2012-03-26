0d7a65da8d2cfaca7f106c4a0c50419c5a5a644f
James Cox <th3james@gmail.com>
2012-03-26 15:06:45 +0100

    Move polygon collection to a separate file

--- app/assets/javascripts/models/polygon.js
+++ app/assets/javascripts/models/polygon.js
@@ -1,34 +1,22 @@
-// Polygon, stored in cartodb
-// belongs_to layer
-App.Polygon = Backbone.Model.extend({
-    defaults: function() {
-        return {
-            'cartodb_id': null,
-            "the_geom": null,
-            "class": null,
-            "layer_id": null
-        };
-    },
+(function() {  
+  // Polygon, stored in cartodb
+  // belongs_to layer
+  App.Polygon = Backbone.Model.extend({
+      defaults: function() {
+          return {
+              'cartodb_id': null,
+              "the_geom": null,
+              "class": null,
+              "layer_id": null
+          };
+      },
 
-    idAttribute: 'cartodb_id',
-    url: '/polygons/',
-    initialize: function() {
-      //_.bindAll(this, '_save');
+      idAttribute: 'cartodb_id',
+      url: '/polygons/',
+      initialize: function() {
+        //_.bindAll(this, '_save');
 
-      this.save();
-    }
-});
-
-App.PolygonCollection = Backbone.Collection.extend({
-    model: App.Polygon,
-    defaults: {
-      'layer_id': null
-    },
-    
-    initialize: function() {
-    },
-
-    fetch: function() {
-      return '/layer/' + this.layer_id + '/polygons';
-    }
-});
+        this.save();
+      }
+  });
+})();
