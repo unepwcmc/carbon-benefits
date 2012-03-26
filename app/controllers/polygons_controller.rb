f9e4e78dd7cebebb337f4a70e750101d6da401c9
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-03-26 11:24:57 +0100

    On load backbone loads the polygons as polygons objects

--- app/controllers/polygons_controller.rb
+++ app/controllers/polygons_controller.rb
@@ -7,4 +7,10 @@ class PolygonsController < ApplicationController
     render :json =>  @polygon.to_json
   end
 
+  def update
+    @polygon = Polygon.find(params[:cartodb_id])
+    @polygon.the_geom = RGeo::GeoJSON.encode(@polygon.the_geom)
+    render :json => @polygon.to_json
+  end
+
 end
