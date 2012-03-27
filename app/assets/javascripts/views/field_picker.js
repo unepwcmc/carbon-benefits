afdcc35a51b728ae6ced9e84a4c0388cfa558766
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-03-27 10:09:46 +0100

    better error messages

--- app/assets/javascripts/views/field_picker.js
+++ app/assets/javascripts/views/field_picker.js
@@ -8,7 +8,7 @@ window.FieldPicker = Backbone.View.extend({
 
   initialize: function() {
     _.bindAll(this, 'render', 'submit');
-
+    this.bus = this.options.bus;
     this.fields = this.options['fields'];
     this.layer_id = this.options['layer_id'];
 
@@ -20,6 +20,7 @@ window.FieldPicker = Backbone.View.extend({
   },
 
   render: function() {
+    var that = this;
     $(this.el).css('height', 'auto');
     $(this.el).append(this.template({fields: this.fields, layer_id: this.layer_id}));
 
@@ -29,29 +30,46 @@ window.FieldPicker = Backbone.View.extend({
       dataType: 'json',
       success: function(res_json){
         var jobId = res_json['job_id'];
-        var timerId = setInterval(function(){
-          $.ajax({
-            url: "/layers/get_job_status?job_id="+jobId,
-            success: function(data){
-              if(data['status'] == 'completed'){
-                alert(data['message']);
-                //TODO now the time to refresh the layer
-                clearInterval(timerId);
-              }
-            },
-            error: function(data){
-              console.log(data);
-              console.log('zonk');
-              clearInterval(timerId);
-            },
-            dataType: "json"
-          });
-        }, 10000);
+        var layerId = res_json['layer_id'];
+        that.pollForUploadProgress(jobId, layerId);
       },
       error: function(xhr){
-        alert('import failed');
+        that.handleUploadError("Upload failed");
       }
     });
+  },
+
+  pollForUploadProgress: function(jobId, layerId){
+    var that = this;
+    var timerId = setInterval(function(){
+      $.ajax({
+        url: "/layers/get_job_status?job_id="+jobId,
+        success: function(data){
+          if(data['status'] == 'completed'){
+            alert(data['message']);
+            //TODO now the time to refresh the layer
+            clearInterval(timerId);
+            that.bus.emit('model:upload_in_progress', layerId);
+          } else if (data['status'] == 'failed'){
+            that.handleUploadError(data['message']);
+            clearInterval(timerId);
+          }
+        },
+        error: function(data){
+          console.log(data);
+          that.handleUploadError("todo");
+          clearInterval(timerId);
+        },
+        dataType: "json"
+      });
+    }, 10000);
+  },
+
+  handleUploadError: function(msg){
+    console.log('upload error');
+    console.log(msg);
+    $(this.el).hide();
+    //TODO set error status somewhere on layer tab?
   }
 
 });
