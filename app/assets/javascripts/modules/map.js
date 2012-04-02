a0f59ae65abab92a455a9cbd30afb547dfd0b7c2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-04-02 09:37:47 +0100

    selective polygon colouring

--- app/assets/javascripts/modules/map.js
+++ app/assets/javascripts/modules/map.js
@@ -420,19 +420,22 @@ App.modules.Map = function(app) {
     show_tile_layer: function(rid, data){
 
       // CartoDB Uploaded Polygon Layer
-      var sql = "SELECT the_geom_webmercator FROM " + window.CARTODB_TABLE + " WHERE layer_id = " + data.id;
+      var sql = "SELECT the_geom_webmercator, class_id FROM " + window.CARTODB_TABLE + " WHERE layer_id = " + data.id;
       sql = sql + data.class_where_clause();
 
       if (this.map.userLayers[data.id] === undefined || this.map.userLayers[data.id].changed){
+        var tileStr = '';
+        _.each(data.classes, function(ele){
+          tileStr += '#polygon_development[class_id=' + ele[2] + ']{polygon-fill:' + ele[1] +';polygon-opacity:0.7;line-width:0} ';
+        }); 
+
         var cartodbLayerParams = {
           map_canvas: 'map_canvas',
           map: this.map.map,
           user_name: 'carbon-tool',
           table_name: window.CARTODB_TABLE,
           query: sql,
-          tile_style: "#" + window.CARTODB_TABLE + "{polygon-fill:" +
-          (data.selected_colour ? data.selected_colour : 'green') +
-          ";polygon-opacity:0.7;line-width:0}",
+          tile_style: tileStr,
           visible: true,
           changed: false
         };
