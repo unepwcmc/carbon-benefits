fda60e00fa286a70cb3b46a78f687a68fbc39f79
James Cox <th3james@gmail.com>
2012-04-02 11:18:44 +0100

    Connect up polygon names collections

--- app/assets/javascripts/views/polygon_filter.js
+++ app/assets/javascripts/views/polygon_filter.js
@@ -4,12 +4,17 @@ $(function() {
     template: JST["templates/polygon_filter"],
 
     initialize: function() {
-      this.render();
-      this.polygon_names_collection = new PolygonNamesCollection({layer_id: this.layer_id});
+      _.bindAll(this, 'render');
+      this.layer_id = this.options.layer_id;
+      this.polygon_names_collection = new PolygonNamesCollection();
+      this.polygon_names_collection.layer_id = this.layer_id;
+      this.polygon_names_collection.fetch({
+        success: this.render
+      });
     },
 
     render: function() {
-      $(this.el).html(this.template({}));
+      $(this.el).html(this.template({polygons: this.polygon_names_collection.models}));
     },
 
     show: function() {
