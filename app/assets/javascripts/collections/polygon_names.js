b0799499b139785f26eed1dc9bdbb7b5189c41e6
James Cox <th3james@gmail.com>
2012-04-03 16:11:40 +0100

    Fixed selection bug on polygons

--- app/assets/javascripts/collections/polygon_names.js
+++ app/assets/javascripts/collections/polygon_names.js
@@ -10,11 +10,16 @@ $(function() {
       var i, il;
       var selectedLayerIds = carbon.work.work.findByLayerId(this.layer_id).get('selected_polygon_ids');
      
+
       this.each(function(polygon) {
+        // Reset polygon...
+        polygon.set({'selected': false});
+
         for (i=0, il=selectedLayerIds.length; i < il; i=i+1) {
           // Set selected if polygon id is in selectedLayerIds
-          console.log(parseInt(selectedLayerIds[i], 10) + ' === ' + polygon.get('cartodb_id') + ' = ' + (parseInt(selectedLayerIds[i], 10) === polygon.get('cartodb_id')));
-          if (parseInt(selectedLayerIds[i], 10) === polygon.get('cartodb_id')) polygon.set({'selected': true});
+          if (parseInt(selectedLayerIds[i], 10) === polygon.get('cartodb_id')) {
+            polygon.set({'selected': true});
+          }
         }
       });
     }
