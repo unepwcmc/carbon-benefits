c130304b66cd85379633fea350221a46e00bc0a8
James Cox <th3james@gmail.com>
2013-03-20 08:49:34 +0000

    Actually include modernizer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require libs/modernizr-2.0.6
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
